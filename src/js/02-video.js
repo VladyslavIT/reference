5c3b9627e9e3344d6cda827d6b26525104bbb811
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-08-06 16:50:21 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,16 +5,16 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector("iframe");
 const player = new Player(iframe);
 
-const currentTime = "videoplayer-current-time";
+const CURRENT_TIME = "videoplayer-current-time";
 
 const saveTime = (data) => {
-  return localStorage.setItem(currentTime, data.seconds);
+  return localStorage.setItem(CURRENT_TIME, data.seconds);
 };
 
 player.on("timeupdate", throttle(saveTime, 1000));
 
 const unpauseVideo = () => {
-  return player.setCurrentTime(localStorage.getItem(currentTime));
+  return player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
 };
 
 unpauseVideo();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle";
 
+const formEL = document.querySelector(".feedback-form");
+const textAreaEL = document.querySelector('form textarea[name = "message"]');
+const inputEl = document.querySelector('form input[name = "email"]');
+
+const CURRENT_VALUE = "feedback-form-state";
+
+const FormValue = {};
+
+onLoadPage();
+
+const onFormVlaue = (event) => {
+  FormValue.email = inputEl.value;
+  FormValue.textarea = textAreaEL.value;
+  localStorage.setItem(CURRENT_VALUE, JSON.stringify(FormValue));
+};
+function onLoadPage() {
+  const saveValue = localStorage.getItem(CURRENT_VALUE);
+  if (saveValue) {
+    const parseValue = JSON.parse(saveValue);
+    inputEl.value = parseValue.email;
+    textAreaEL.value = parseValue.textarea;
+  } else {
+    inputEl.value = "";
+    textAreaEL.value = "";
+  }
+};
+
+const onClearFormValue = (event) => {
+  event.preventDefault();
+  let { email, message } = event.currentTarget;
+  email = inputEl.value;
+  message = textAreaEL.value;
+  console.log({ email, message });
+  event.currentTarget.reset();
+  localStorage.removeItem(CURRENT_VALUE);
+};
+formEL.addEventListener("input", throttle(onFormVlaue, 500));
+formEL.addEventListener("submit", onClearFormValue);
